40a174e21609d5202a97b0422de05cedf2806de2
mwenda-eric <ericmwenda5@gmail.com>
2018-04-10 05:49:47 +0000

    [Add Hopage tests]

--- src/__tests__/components/Home.jsx
+++ src/__tests__/components/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Register from '../../Components/Register';
+import Home from '../../Components/Home';
 import { MemoryRouter as Router } from 'react-router-dom';
 // import renderer for snapshot testing
 import renderer from 'react-test-renderer';
 
-describe('Register', () => {
+describe('Home', () => {
   it('renders without crashing', () => {
     const pathname = {
       pathname: "/"
     };
     const rendered = renderer.create(
-      <Router><Register location={pathname} /></Router>
+      <Router><Home location={pathname} /></Router>
     );
     expect(rendered.toJSON()).toMatchSnapshot();
   });
