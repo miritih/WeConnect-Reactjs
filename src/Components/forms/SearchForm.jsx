e57a470d67be34e1e37d5f4d319fa59803a409c0
miritih <ericmwenda5@gmail.com>
2018-07-29 08:03:30 +0300

    [FT] Add search functionality on business list page

--- src/Components/forms/SearchForm.jsx
+++ src/Components/forms/SearchForm.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
-class SearchForm extends React.Component {
-	render() {
-		return (
+import { PropTypes } from 'prop-types';
+
+export const SearchForm = ({onSearch, onChange, value}) => {
+	return (
+		<div className="container">
 			<div className="row">
-				<div className="col-md-1 col-sm-12 col-xm-12"></div>
-				<div className="col-md-10 col-sm-12 col-xm-12">
+				<div className="col-md-12 col-sm-12 col-xm-12">
 					<div className="card">
 						<div className="card-header">
-							<h5 className="card-title text-center">Find and connect to a businesss</h5>
+							<h5 className="card-title text-center">Search Businesses</h5>
 						</div>
 						<div className="card-body">
-							<form className="form-inline">
-								<label className="sr-only">Business Name</label>
-								<input type="text" className="form-control ml-auto" placeholder="Search by name" name="bsname" />
-								<label className="sr-only">Location</label>
-								<input type="text" className="form-control ml-auto" placeholder="Search by Location" name="location" />
-								<label className="sr-only">Category</label>
-								<input type="text" className="form-control ml-auto" placeholder="Search by Catogory" name="category" />
-								<button className="btn btn-success ml-auto">Search</button>
+							<form onSubmit={onSearch} className="form-inline">
+								<div className="input-group col-md-8 offset-md-2">
+									<input 
+										className="form-control form-control-lg" 
+										type="search"
+										onKeyUp={onSearch}
+										value={value}
+										onChange={onChange}
+										placeholder="filter by business Name Nategory or Location" 
+										id="example-search-input"
+									/>
+									<span className="input-group-append">
+										<button className="btn btn-outline-info" type="submit">
+											<i className="fa fa-search"></i>
+										</button>
+									</span>
+								</div>
 							</form>
 						</div>
 					</div>
 				</div>
-				<div className="col-md-1 col-sm-12 col-xm-12"></div>
 			</div>
-		);
-	}
-}
-
+		</div>
+	);
+};
+SearchForm.propTypes = {
+	onSearch:PropTypes.func.isRequired, 
+	onChange: PropTypes.func.isRequired,
+	value: PropTypes.string.isRequired,
+};
 export default SearchForm;
